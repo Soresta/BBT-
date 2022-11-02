8a7eaeb4e80d129802886a777eaf9feb204756c8
Akif <112137968+Soresta@users.noreply.github.com>
2022-11-02 20:38:25 +0300

    Update IkinciDerecedenDenklem.java

--- BBT Dersi/BBT/IkinciDerecedenDenklem.java
+++ BBT Dersi/BBT/IkinciDerecedenDenklem.java
@@ -5,20 +5,20 @@ public class IkinciDerecedenDenklem {
         Scanner read = new Scanner(System.in);
         Double a, b, c, x1, x2, alfa;
 
-        System.out.println("a'nın değerini giriniz: ");
+        System.out.print("a'nın değerini giriniz: ");
         a = read.nextDouble();
 
-        System.out.println("b'nın değerini giriniz: ");
+        System.out.print("b'nın değerini giriniz: ");
         b = read.nextDouble();
 
-        System.out.println("c'nın değerini giriniz: ");
+        System.out.print("c'nın değerini giriniz: ");
         c = read.nextDouble();
 
         alfa = (b * b) - (4 * a * c);
         if (alfa >= 0) {
             x1 = (-b + Math.sqrt((Double) alfa)) / 2 * a;
             x2 = (-b - Math.sqrt((Double) alfa)) / 2 * a;
-            System.out.println(x1 + " " + x2);
+            System.out.println("x1 = "x1 + ", x2 =" + x2);
         } else {
             System.out.println("Alfa 0'dan küçük: Reel kök yoktur!");
         }
