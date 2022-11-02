6cc5c1d8c4051c82d650595dc3ec68267afcdc50
Akif <112137968+Soresta@users.noreply.github.com>
2022-11-02 21:08:15 +0300

    Update MutlakDeger.java

--- BBT Dersi/BBT/MutlakDeger.java
+++ BBT Dersi/BBT/MutlakDeger.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class MutlakDeger {
     public static void main(String[] args) {
-
+        //klavyeden girilen sayının mutlak değerini ekrana yazdıran programı yazınız..
+        
         Scanner read = new Scanner(System.in);
         System.out.print("Sayı giriniz: ");
         int number = read.nextInt();
 
         if (number < 0) {
             int numberA = -number;
-            System.out.println(number + ". Sayısının mutlak değeri : " + numberA);
+            System.out.println(number + " Sayısının mutlak değeri : " + numberA);
         } else {
-            System.out.println(number + ". Sayısının mutlak değeri : " + number);
+            System.out.println(number + " Sayısının mutlak değeri : " + number);
         }
     }
 }
