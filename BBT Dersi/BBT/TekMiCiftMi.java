750ef28e9c2e91aaa56f45b4d781c4c9e89be4ab
Akif <112137968+Soresta@users.noreply.github.com>
2022-11-02 20:53:37 +0300

    Update TekMiCiftMi.java

--- BBT Dersi/BBT/TekMiCiftMi.java
+++ BBT Dersi/BBT/TekMiCiftMi.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class TekMiCiftMi {
     public static void main(String[] args) {
+        //Girilen sayının Tek mi Çift mi olduğunu ekrana yazan program..
         Scanner read = new Scanner(System.in);
 
         System.out.print("Sayı giriniz: ");
         int number = read.nextInt();
 
         if (number % 2 == 0) {
-            System.out.println(number + ". Sayısı Çift sayıdır.");
+            System.out.println(number + " Sayısı Çift sayıdır.");
         } else {
-            System.out.println(number + ". Sayısı Tek sayıdır.");
+            System.out.println(number + " Sayısı Tek sayıdır.");
         }
     }
 }
